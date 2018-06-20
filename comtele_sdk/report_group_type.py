5901f418ebe195b9ee6f539b5af56229176a2a19
Diego Vieira <divieira.silva@gmail.com>
2018-06-20 13:04:32 -0300

    adicionado gerenciamento de blacklist

--- comtele_sdk/report_group_type.py
+++ comtele_sdk/report_group_type.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
+
 class ReportGroupType(Enum):
     MONTHLY = 0
     DAILY = 1
-
-
--- comtele_sdk/textmessage_service.py
+++ comtele_sdk/textmessage_service.py
@@ -1,22 +1,26 @@
-import requests, json
+import requests
+import json
 from comtele_sdk.delivery_status import DeliveryStatus
 from comtele_sdk.report_group_type import ReportGroupType
+
 
 class TextMessageService(object):
     def __init__(self, api_key):
         self.__api_key = api_key
 
-    def send(self, sender, content, receivers):        
-        payload = {'sender': sender, 'content': content, 'receivers': ','.join(receivers)}
+    def send(self, sender, content, receivers):
+        payload = {'sender': sender, 'content': content,
+                   'receivers': ','.join(receivers)}
 
-        request = requests.post('https://sms.comtele.com.br/api/v2/send', 
-                         data=json.dumps(payload),
-                         headers={'content-type': 'application/json', 'auth-key': self.__api_key})
+        request = requests.post('https://sms.comtele.com.br/api/v2/send',
+                                data=json.dumps(payload),
+                                headers={'content-type': 'application/json', 'auth-key': self.__api_key})
 
         return request.json()
 
     def schedule(self, sender, content, schedule_date, receivers):
-        payload = {'sender': sender, 'content': content, 'scheduleDate': schedule_date, 'receivers': ','.join(receivers)}
+        payload = {'sender': sender, 'content': content,
+                   'scheduleDate': schedule_date, 'receivers': ','.join(receivers)}
 
         request = requests.post('https://sms.comtele.com.br/api/v2/schedule',
                                 data=json.dumps(payload),
@@ -26,23 +30,26 @@ class TextMessageService(object):
 
     def get_detailed_report(self, start_date, end_date, delivery_status):
 
-        delivery_status_as_string = self.__delivery_status_to_string(delivery_status)
+        delivery_status_as_string = self.__delivery_status_to_string(
+            delivery_status)
 
-        payload = {'startDate': start_date, 'endDate': end_date, 'delivered': delivery_status_as_string}        
-        request = requests.get('https://sms.comtele.com.br/api/v2/detailedreporting', 
-                         params=payload, 
-                         headers={'content-type': 'application/json', 'auth-key': self.__api_key})
-        
+        payload = {'startDate': start_date, 'endDate': end_date,
+                   'delivered': delivery_status_as_string}
+        request = requests.get('https://sms.comtele.com.br/api/v2/detailedreporting',
+                               params=payload,
+                               headers={'content-type': 'application/json', 'auth-key': self.__api_key})
+
         return request.json()
 
     def get_consolidated_report(self, start_date, end_date, group_type):
         group_type_as_string = self.__report_group_type_to_string(group_type)
 
-        payload = {'startDate': start_date, 'endDate': end_date, 'group': group_type_as_string}        
-        request = requests.get('https://sms.comtele.com.br/api/v2/consolidatedreporting', 
-                         params=payload, 
-                         headers={'content-type': 'application/json', 'auth-key': self.__api_key})
-        
+        payload = {'startDate': start_date,
+                   'endDate': end_date, 'group': group_type_as_string}
+        request = requests.get('https://sms.comtele.com.br/api/v2/consolidatedreporting',
+                               params=payload,
+                               headers={'content-type': 'application/json', 'auth-key': self.__api_key})
+
         return request.json()
 
     def __delivery_status_to_string(self, delivery_status):
@@ -62,4 +69,3 @@ class TextMessageService(object):
             return 'false'
 
         return 'true'
-            
